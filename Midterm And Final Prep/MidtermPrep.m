748f0cd5ae2be72f71f16c043152cb80590d3b2d
David Li <pandabeareee@gmail.com>
2017-10-19 13:09:22 -0700

    adding most of the midterm prep files

--- Midterm And Final Prep/MidtermPrep.m
+++ Midterm And Final Prep/MidtermPrep.m
@@ -1,5 +1,7 @@
 %% Midterm PREP Script
 % Chapter 6 --- Stabability Question 1, Nise Textbook
+clear all
+close all
 %% 
 % s^5 +3s^4+5s^3 + 4s^2+s+3
 polyVector = [1 3 5 4 1 3]
@@ -16,7 +18,7 @@ syms s
 %%
 % syms a b c EPS;
 syms a b c EPS;
-ra=routh([1 a b c],EPS);
+ra=routh([1 a b c],EPS)
 
 %% Question 12
 % K(s+2)/s(s-1)(s+3)
@@ -33,38 +35,38 @@ ra4 = routh([1 5 4+K 6*K],EPS)
 
 %% Question 22
 syms a b c EPS;
-ra5 = routh([1 K-b -a],EPS
+ra5 = routh([1 K-b -a],EPS)
 
 %% Example 7.2 from Nise
 syms s
-G(s) = 120*(s+2)/((s+3)*(s+4))
-CSlashR = simplify(G(s)/(1+G(s)))
+Gs = 120*(s+2)/((s+3)*(s+4))
+CSlashR = simplify(Gs/(1+Gs))
 polyVector = [1 127 252]
 routh([1 127 252],EPS)
-limit(G(s),s,0)
-limit(s*G(s),s,0)
+limit(Gs,s,0)
+limit(s*Gs,s,0)
 
-limit(s^2*G(s),s,0)
+limit(s^2*Gs,s,0)
 
 %% Example 7.3 from Nise
 syms s
-G(s) =100*(s+2)*(s+6)/(s*(s+3)*(s+4))
-CSlashR = simplify(G(s)/(1+G(s)))
+Gs =100*(s+2)*(s+6)/(s*(s+3)*(s+4))
+CSlashR = simplify(Gs/(1+Gs))
 polyVector = [1 107 812 1200]
 routh(polyVector, EPS)
 %R(s) = 5*1/s
-limit(5/(1+G(s)),s,0)
+limit(5/(1+Gs),s,0)
 %%
 
 %% Example 7.4 from Nise
-G(s) = 10*(s+20)*(s+30)/((s*(s+25)*(s+35)))
-CSlashR = simplify(G(s)/(1+G(s)))
+Gs = 10*(s+20)*(s+30)/((s*(s+25)*(s+35)))
+CSlashR = simplify(Gs/(1+Gs))
 polyVector = [1 70 1375 600]
 routh(polyVector, EPS)
 
 %%
-G(s) = 10*(s+20)*(s+30)/((s^2*(s+25)*(s+35)*(s+50)))
-CSlashR = simplify(G(s)/(1+G(s)))
+Gs = 10*(s+20)*(s+30)/((s^2*(s+25)*(s+35)*(s+50)))
+CSlashR = simplify(Gs/(1+Gs))
 polyVector = [1 110 3875 43760 500 6000]
 routh(polyVector, EPS)
 
@@ -99,3 +101,122 @@ Kp=dcgain(G)
 estep=1/(1+Kp)
 T=feedback(G,1);
 poles=pole(T)
+
+%% Chapter 7 or 8 root Locus Plots
+% Root Locus Plot
+h = tf([2 5 1],[1 2 3]);
+rlocus(h)
+snapnow;
+%%
+% Example from textbook
+syms s
+x = s*(s+1)*(s+2)*(s+4)
+den = sym2poly(x)
+num = (s+3)
+num = sym2poly(num)
+h = tf([num],[den])
+rlocus(h)
+
+%%
+% $$ $$
+%
+x = (s+2)*(s+4)*(s+6)
+den = sym2poly(x)   
+h = tf([1],[den])
+rlocus(h)
+
+%%
+% 
+syms s K EPS
+x =s^4+ 7*s^3+14*s^2+(8+K)*s+3*K
+polyVec = [1 7 14 8+K 3*K]
+xD = routh(polyVec, EPS)
+xD = simplify(xD)
+
+%%
+%
+polyVec = [1 6 8]
+routh(polyVec,EPS)
+% stable function
+rlocus(tf([1 4 20],[1 6 8]))
+
+%%
+% Question 8--4 
+num= tf([1 2/3])
+den = tf([1 6 0 0])
+
+sys = tf([1 2/3], [1 6 0 0])
+rlocus(sys)
+
+%%
+% Example NISE
+syms s
+top =(s+3)*(s+4)
+polyTop = sym2poly(top)
+
+bot = (s+2)*(s+1)
+polyBot = sym2poly(bot)
+num= tf([polyTop])
+den = tf(polyBot)
+
+sys = tf([1 7 12], [1 3 2])
+rlocus(sys)
+
+simplify(diff((s^2+3*s+2)/(s^2+7*s+12),s))
+solve(s^2 + 10*s + 11==0,s)
+
+%%
+% Also, see
+% <https://www.allaboutcircuits.com/technical-articles/an-introduction-to-control-systems-designing-a-pid-controller-using-matlabs/>
+%- 14^(1/2) - 5
+%ans =
+%   -8.7417
+%14^(1/2) - 5
+%ans =
+%   -1.2583
+
+%%
+% More examples for stability
+numg=[1];
+deng=([-7 -9]);
+G=tf(numg,deng);
+T=feedback(G,1);
+%%
+% finding K for s(s+1)(s+2)
+clear all
+syms s K EPS
+
+NUMBER = s*(s+1)*(s+2) + K
+NUmber = NUMBER
+% s^3 + 3*s^2 + 2*s
+polyVec = [1 3 2 K]
+routh(polyVec, EPS)
+%%
+% checking for stability
+syms s K EPS
+
+polyVec = [1 2 0.75+K 3.15*K]
+routh(polyVec,EPS)
+
+%%
+% Another question
+% G(s) =K / (s)*(s+2)*(s+4)
+
+sys = tf([1], [1 6 8 0])
+rlocus(sys)
+%%
+% 1 / s^3+5*s^2+20*s+6
+polyVec = [1 5 20 6]
+routh(polyVec,EPS)
+p = s^3+5*s^2+20*s+6
+% Roots can only be solved numerically in this case, not exactly, therefore
+% the symbolic expression to evaluate roots will not work, must have a
+% vector polynomial
+vectorPoly = sym2poly(p)
+roots(vectorPoly)
+
+%%
+% Go = 1/ (s+1)^3
+(s+1)^3
+a = s^3 + 3*s^2 + 3*s + 1
+polyVec = [1 3 3 1]
---
+++ Midterm And Final Prep/html/The Root Locus Rules_files/convertGifToPng.py
@@ -0,0 +1,85 @@
+#! python3
+   # combinePdfs.py - Combines all the PDFs in the current working directory into
+   # into a single PDF.
+
+# This script requires the user to change the filename and working directory, ideally I would be able to include this requirements as arguments, but that will be added later.
+import PyPDF2, os,sys
+
+print("Running the script called, ", sys.argv[0])
+## Use a website like http://www.unit-conversion.info/texttools/replace-text/
+# This will allow for the proper slashing
+#var = "C:/Users/wu/Downloads/University Fun/School/Summer 2017/ELEC 310/Final/Assignments"
+#os.chdir(var);
+#filename = 'epictest.pdf'
+   # Get all the PDF filenames.
+
+import os
+import glob, os
+os.chdir(os.path.dirname(sys.argv[0]))
+print(os.path.dirname(sys.argv[0]))
+gifFiles = []
+numOfConvertedGifFiles = 0
+for filename in os.listdir('.'):
+    if filename.endswith('.gif'):
+        filename = os.path.splitext(filename)[0]
+        gifFiles.append(filename)
+gifFiles.sort(key=str.lower)
+
+# See https://stackoverflow.com/questions/19230991/image-open-cannot-identify-image-file-python
+# path = "C:/Users/wu/Downloads/University Fun/School/Fall 2017/CENG 356/CENG356/MidTermPrep/Organization of Computer Systems_ Pipelining_files/" 
+#path =  os.chdir(os.path.dirname(sys.argv[0])) 
+from PIL import Image
+for filename in gifFiles:
+    pathToFile = filename
+    print(pathToFile)
+    img = Image.open(pathToFile +'.gif')
+    img.load()
+    img.save(filename+'.png','PNG')
+    numOfConvertedGifFiles = numOfConvertedGifFiles  +1
+print('The script is complete by ', numOfConvertedGifFiles, ' merged files.')
+
+
+#from PIL import Image
+#import glob, os
+#
+##size = 128, 128
+#
+#for infile in glob.glob("*.gif"):
+#    file, ext = os.path.splitext(infile)
+#    im = Image.open(infile)
+#    #im.thumbnail(size)
+#    im.save(file + ".png", "PNG")
+#
+#pdfFiles = []
+## gets the pdfs in the directory of combinePdfs.py
+##for filename in os.listdir('.'): 
+#numOfMergedFiles = 0
+#for filename in os.listdir('.'): 
+#   if filename.endswith('.gif'): #consider adding regular expressions
+#       pdfFiles.append(filename)
+#pdfFiles.sort(key=str.lower)
+#
+#pdfWriter = PyPDF2.PdfFileWriter()
+## Loop through all the PDF files.
+#for filename in pdfFiles:
+#    pdfFileObj = open(filename, 'rb')
+#    pdfReader = PyPDF2.PdfFileReader(pdfFileObj)
+#    # Loop through all the pages (except the first) and add them.
+#
+#    pageObj = pdfReader.getPage(0)
+#    pdfWriter.addPage(pageObj)
+#    fun = pdfWriter.getNumPages() -1
+#    parent = pdfWriter.addBookmark(filename,fun) # add parent bookmark
+#    numOfMergedFiles = numOfMergedFiles + 1
+#    #for pageNum in range(1, pdfReader.numPages):
+#    # Loop through all the pages (except the first) and add them.
+#    for pageNum in range(1, pdfReader.numPages):
+#        pageObj = pdfReader.getPage(pageNum)
+#        pdfWriter.addPage(pageObj)
+#
+#
+#pdfOutput = open('ELEC310MergedAssnSoln.pdf', 'wb')
+#pdfWriter.write(pdfOutput)
+#pdfOutput.close()
+
+#sys.exit(0)
